284384668b108d628a91d9c47cb4b1a2696add75
Xinhao Liang <xinhao128@hotmail.com>
2019-02-05 21:29:48 -0800

    Completed Twitter Part I

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,67 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Xinhao Liang on 2/5/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+    
+    var tweetArray = [NSDictionary]()
+    var numberOfTweet: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadTweet()
+    }
+    
+    func loadTweet() {
+        let myURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": 10]
+        TwitterAPICaller.client?.getDictionariesRequest(url: myURL, parameters: myParams, success: {(tweets: [NSDictionary]) in
+            
+            self.tweetArray.removeAll()
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            self.tableView.reloadData()
+        }, failure: { (Error) in
+            print("Could not retrieve tweets!!")
+        })
+    }
+
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        cell.userNameLabel.text = user["name"] as? String
+        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
+        
+        let imageURL = URL(string: (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageURL!)
+        if let imageData = data {
+            cell.profileImageView.image = UIImage(data: imageData)
+        }
+        return cell
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return tweetArray.count
+    }
+
+}
